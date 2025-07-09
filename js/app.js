e97e1b7c2f1a9e79f8e3257d2990f734249494cf
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-07-09 15:25:37 +0200

    feat: snack 3 completato

--- js/app.js
+++ js/app.js
@@ -1,59 +1,59 @@
 const books = [
-  { 
-	  title: "React Billionaire", 
-	  pages: 250, 
-	  author: {
-		  name: 'Alice',
-		  age: 35
-	  },
-	  available: false,
-	  price: '101€',
-	  tags: ['advanced', 'js', 'react', 'senior']
-  },
-  { 
-	  title: "Advanced JS", 
-	  pages: 500, 
-	  author: {
-		  name: 'Bob',
-		  age: 20
-	  },
-	  available: true,
-	  price: '25€',
-	  tags: ['advanced', 'js', 'mid-senior']
-  },
-  { 
-	  title: "CSS Secrets", 
-	  pages: 320, 
-	  author: {
-		  name: 'Alice',
-		  age: 17
-	  },
-	  available: true,
-	  price: '8€',
-	  tags: ['html', 'css', 'junior']
-  },
-  { 
-	  title: "HTML Mastery", 
-	  pages: 200, 
-	  author: {
-		  name: 'Charlie',
-		  age: 50
-	  },
-	  available: false,
-	  price: '48€',
-	  tags: ['html', 'advanced', 'junior', 'mid-senior']
-  },
+    {
+        title: "React Billionaire",
+        pages: 250,
+        author: {
+            name: 'Alice',
+            age: 35
+        },
+        available: false,
+        price: '101€',
+        tags: ['advanced', 'js', 'react', 'senior']
+    },
+    {
+        title: "Advanced JS",
+        pages: 500,
+        author: {
+            name: 'Bob',
+            age: 20
+        },
+        available: true,
+        price: '25€',
+        tags: ['advanced', 'js', 'mid-senior']
+    },
+    {
+        title: "CSS Secrets",
+        pages: 320,
+        author: {
+            name: 'Alice',
+            age: 17
+        },
+        available: true,
+        price: '8€',
+        tags: ['html', 'css', 'junior']
+    },
+    {
+        title: "HTML Mastery",
+        pages: 200,
+        author: {
+            name: 'Charlie',
+            age: 50
+        },
+        available: false,
+        price: '48€',
+        tags: ['html', 'advanced', 'junior', 'mid-senior']
+    },
 ];
 
 // Snack 1
 
-const longBooks = books.filter(book =>{
+const longBooks = books.filter(book => {
     return book.pages > 300
 })
 
 console.log(longBooks)
 
-const longBooksTitles = longBooks.map(book =>{
+const longBooksTitles = longBooks.map(book => {
     return book.title
 })
 
@@ -61,24 +61,46 @@ console.log(longBooksTitles)
 
 // Snack 2 
 
-const availableBooks = books.filter(book =>{
+const availableBooks = books.filter(book => {
     return book.available === true
 })
 
 console.log(availableBooks)
 
-const discountedBooks =  structuredClone(availableBooks)
+const discountedBooks = structuredClone(availableBooks)
 
-discountedBooks.map(book =>{
-    
-    book.price = parseInt(book.price) * (1 -0.20)
-    return {...availableBooks}
+discountedBooks.map(book => {
+
+    book.price = parseInt(book.price) * (1 - 0.20)
+    return { ...availableBooks }
 })
 
 console.log(discountedBooks)
 
-const fullPricedBook = discountedBooks.find(book =>{
+const fullPricedBook = discountedBooks.find(book => {
     return book.price % 1 === 0
 })
 
 console.log(fullPricedBook)
+
+// Snack 3 
+
+const authors = books.map(book => {
+    return book.author
+})
+
+console.log(authors)
+
+const areAuthorsAdults = authors.every(a => a.age >= 18)
+
+authors.sort((a, b) => {
+    if (areAuthorsAdults) {
+        return a.age - b.age
+    } else {
+        return b.age - a.age
+    }
+})
+
+console.log(authors)
+
+
